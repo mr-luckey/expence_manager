bf886e243ba741144797960e877721e01fb2eb24
EngineerHamza123 <mh9257964@gmail.com>
2024-06-11 18:14:02 +0500

    Ali
    code is running

--- lib/Views/Add_Goals.dart
+++ lib/Views/Add_Goals.dart
@@ -1,8 +1,8 @@
-import 'package:expence_manager/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:expence_manager/widgets/app_bar.dart';
 
 class AddGoals extends StatefulWidget {
-  const AddGoals({super.key});
+  const AddGoals({Key? key}) : super(key: key);
 
   @override
   State<AddGoals> createState() => _AddGoalsState();
@@ -12,6 +12,9 @@ class _AddGoalsState extends State<AddGoals> {
   final TextEditingController _amountController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   final TextEditingController _contributionController = TextEditingController();
+  bool _isDropdownOpen = false;
+  List<String> _contributions = ['Daily', 'Weekly', 'Monthly', 'Yearly'];
+  String _selectedContribution = 'Daily'; // Set an initial value
 
   @override
   Widget build(BuildContext context) {
@@ -24,88 +27,95 @@ class _AddGoalsState extends State<AddGoals> {
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Income Title',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Income Title',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Container(
-                height: 40, // Set the height of the input field
-                child: TextField(
-                  controller: _amountController,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    hintText: '',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Container(
+                  height: 40, // Set the height of the input field
+                  child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: '',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Amount',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Amount',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Container(
-                height: 40, // Set the height of the input field
-                child: TextField(
-                  controller: _descriptionController,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    hintText: '',
-                    suffixIcon: Icon(Icons.attach_money),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Container(
+                  height: 40, // Set the height of the input field
+                  child: TextField(
+                    controller: _descriptionController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: '',
+                      suffixIcon: Icon(Icons.attach_money),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Contribution Type',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Contribution Type',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Container(
-                height: 40, // Set the height of the input field
-                child: TextField(
-                  controller: _contributionController,
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(
-                    hintText: '',
-                    suffixIcon: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.arrow_drop_down),
-                      ],
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Container(
+                  height: 40, // Set the height of the input field
+                  child: DropdownButtonFormField<String>(
+                    value: _selectedContribution,
+                    items: _contributions.map((String contribution) {
+                      return DropdownMenuItem<String>(
+                        value: contribution,
+                        child: Text(contribution),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedContribution = newValue!;
+                        _contributionController.text = newValue;
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
